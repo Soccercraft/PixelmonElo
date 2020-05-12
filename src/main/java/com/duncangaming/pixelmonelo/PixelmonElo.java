1237d42ac1f7fac39fa5b1f2a2d519d1f9a62d1a
soccercraft <wijnbergduncan@gmail.com>
2020-05-11 20:43:48 -0700

    Version 1.1

--- src/main/java/com/duncangaming/pixelmonelo/PixelmonElo.java
+++ src/main/java/com/duncangaming/pixelmonelo/PixelmonElo.java
@@ -6,6 +6,7 @@ import com.pixelmonmod.pixelmon.api.events.battles.BattleEndEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -16,6 +17,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = PixelmonElo.MODID, name = PixelmonElo.NAME, version = PixelmonElo.VERSION, dependencies = PixelmonElo.DEPENDENCIES)
 public class PixelmonElo
 {
+    Events events = new Events();
     public static final String MODID = "pixelmonelo";
     public static final String NAME = "Pixelmon Elo";
     public static final String VERSION = "1.0";
@@ -32,10 +34,7 @@ public class PixelmonElo
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        for (EntityPlayerMP p = Minecraft.getMinecraft()) {
-
-        }
-        // some example code
+        MinecraftForge.EVENT_BUS.register(events);
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
